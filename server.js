251aefa59ed0d80fcad1566a38fcee3e0bbf0cf1
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2018-09-03 09:26:08 -0400

    html pages added

--- server.js
+++ server.js
@@ -49,9 +49,8 @@ if (process.env.NODE_ENV === "test") {
 db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
     console.log(
-      "==>  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
+      `Listening on ${PORT}`
+    
     );
   });
 });
